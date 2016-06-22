ef809d00effb280c606f60fa04f6293edd2d146a
EC2 Default User <ec2-user@ip-172-30-0-226.ec2.internal>
2016-06-22 01:41:50 -0400

    init

---
+++ amimoto-plugin-dashboard.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Amimoto-plugin-dashboard
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: amimoto-plugin-dashboard
+ * Domain Path: /languages
+ * @package Amimoto-plugin-dashboard
+ */
