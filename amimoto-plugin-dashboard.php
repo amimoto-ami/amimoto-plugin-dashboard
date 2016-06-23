74e39f34baa90885da6e25abbdfb7f4d3472465b
hideokamoto <hide.okamoto@digitalcube.jp>
2016-06-23 17:41:54 +0900

    update readme

--- amimoto-plugin-dashboard.php
+++ amimoto-plugin-dashboard.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name: Amimoto-plugin-dashboard
- * Version: 0.1-alpha
- * Description: PLUGIN DESCRIPTION HERE
- * Author: YOUR NAME HERE
- * Author URI: YOUR SITE HERE
- * Plugin URI: PLUGIN SITE HERE
+ * Plugin Name: AMIMOTO Plugin Dashboard
+ * Version: 0.0.1
+ * Description: Controle AMIMOTO helper plugins
+ * Author: hideokamoto,amimotoami
+ * Author URI: https://amimoto-ami.com
+ * Plugin URI: https://github.com/amimoto-ami/amimoto-plugin-dashboard/
  * Text Domain: amimoto-plugin-dashboard
  * Domain Path: /languages
  * @package Amimoto-plugin-dashboard
