eb718ee751ae3c46e0bae68bf5de20c5cda41fdf
Daniel Olson <emaildano@gmail.com>
2017-12-14 15:52:43 -0500

    updated plugin template array

--- module/view/admin.php
+++ module/view/admin.php
@@ -101,19 +101,19 @@ class Amimoto_Dash_Admin extends Amimoto_Dash_Component {
 	 * @return array
 	 * @since 0.0.1
 	 */
-	private function _amimoto_plugin_list_template(
-		$title = '',
-		$thumbnail = '',
-		$slug = '',
-		$short_description = ''
-	) {
+	private function _amimoto_plugin_list_template( $args = array(
+		'slug' => null,
+		'title' => null,
+		'thumbnail' => null,
+		'desc' => null
+	)) {
 
 		// Load WP Core Thickbox Scripts
 		wp_enqueue_style('thickbox');
 		wp_enqueue_script('thickbox');
 
 		$details_link = self_admin_url(
-			'plugin-install.php?tab=plugin-information&amp;plugin=' . $slug .
+			'plugin-install.php?tab=plugin-information&amp;plugin=' . $args['slug'] .
 			'&amp;TB_iframe=true&amp;width=600&amp;height=550'
 		);
 
@@ -121,29 +121,32 @@ class Amimoto_Dash_Admin extends Amimoto_Dash_Component {
 			'update.php?action=install-plugin&plugin=c3-cloudfront-clear-cache&_wpnonce=19d34c3b3c'
 		);
 
+		// echo '<pre>' . print_r(get_plugins()) . '</pre>';
+		// echo '<pre>' . print_r(is_plugin_active('jetpack/jetpack.php')) . '</pre>';
+
 		$wp_plugin_html = '
 			<div class="plugin-card plugin-card-akismet">
 				<div class="plugin-card-top">
 					<div class="name column-name">
 						<h3>
 						<a href="'.esc_url( $details_link ).'" class="thickbox open-plugin-details-modal">
-							'.$title.'
-							<img src="'.$thumbnail.'" class="plugin-icon" alt="">
+							'.$args['title'].'
+							<img src="'.$args['thumbnail'].'" class="plugin-icon" alt="">
 						</a>
 					</h3>
 					</div>
 					<div class="action-links">
 						<ul class="plugin-action-buttons">
 							<li>
-								<a class="install-now button" data-slug="'.$slug.'" href="'.$actions_link.'" aria-label="Install bbPress 2.5.14 now" data-name="bbPress 2.5.14">Install Now</a>
+								<a class="install-now button" data-slug="'.$args['slug'].'" href="'.$actions_link.'" aria-label="Install bbPress 2.5.14 now" data-name="bbPress 2.5.14">Install Now</a>
 							</li>
 							<li>
-								<a href="'.get_admin_url('/').'plugin-install.php?tab=plugin-information&amp;plugin='.$slug.'&amp;TB_iframe=true&amp;width=600&amp;height=550" class="thickbox open-plugin-details-modal" aria-label="More information about bbPress 2.5.14" data-title="bbPress 2.5.14">More Details</a>
+								<a href="'.get_admin_url('/').'plugin-install.php?tab=plugin-information&amp;plugin='.$args['slug'].'&amp;TB_iframe=true&amp;width=600&amp;height=550" class="thickbox open-plugin-details-modal" aria-label="More information about bbPress 2.5.14" data-title="bbPress 2.5.14">More Details</a>
 							</li>
 						</ul>
 					</div>
 					<div class="desc column-description">
-						<p>'.$short_description.'</p>
+						<p>'.$args['desc'].'</p>
 					</div>
 				</div>
 			</div>
@@ -181,12 +184,13 @@ class Amimoto_Dash_Admin extends Amimoto_Dash_Component {
 		$html .= '<div class="wp-list-table">';
 
 		foreach ($amimoto_plugins as $p) {
-			$html .= $this->_amimoto_plugin_list_template(
-				$p['name'],
-				$p['thumbnail'],
-				$p['slug'],
-				$p['short_description']
+			$args = array(
+				'title' => $p['name'],
+				'desc' => $p['short_description'],
+				'thumbnail' => $p['thumbnail'],
+				'slug' => $p['slug']
 			);
+			$html .= $this->_amimoto_plugin_list_template($args);
 		}
 
 		$html .= '</div>';
@@ -194,97 +198,6 @@ class Amimoto_Dash_Admin extends Amimoto_Dash_Component {
 		$html .= '</div></div></form>';
 
 		return $html;
-	}
-
-	/**
-	 *  Get plugin list that uninstalled amimoto plugins
-	 *
-	 * @access private
-	 * @return string(html)
-	 * @since 0.0.1
-	 */
-	private function _get_uninstalled_amimoto_plugin_html() {
-		$html  = '';
-		foreach ( $this->amimoto_uninstalled_plugins as $plugin_name => $plugin_url ) {
-			if ( 'Nginx Cache Controller on WP.org' == $plugin_name ) {
-				if ( $this->is_exists_ncc() ) {
-					continue;
-				}
-				$plugin_name = 'Nginx Cache Controller';
-			}
-			$plugin_install_url = "./plugin-install.php?tab=search&type=term&s=". urlencode( $plugin_name );
-			$description = $this->_get_amimoto_plugin_description( $plugin_name );
-			$for_use = $this->_get_amimoto_plugin_for_use( $plugin_name );
-			$html .= "<tr class='inactive'><td>";
-			$html .= "<h2>{$plugin_name}</h2>";
-			$html .= '<dl><dt><b>'. __( 'For use:', self::$text_domain ). "</b></dt><dd>{$for_use}</dd>";
-			$html .= '<dl><dt><b>'. __( 'Plugin Description:', self::$text_domain ). "</b></dt><dd>{$description}</dd>";
-			$html .= '</dl>';
-			$html .= "<a class='install-now button' href='{$plugin_install_url}' aria-label='Install {$plugin_name} now' data-name='{$plugin_name}'>Install Now</a>";
-			$html .= '</td></tr>';
-		}
-		return $html;
-	}
-
-	/**
-	 *  Get amimoto plugin description
-	 *
-	 * @access private
-	 * @param (string) $plugin_name
-	 * @return string
-	 * @since 0.0.1
-	 */
-	private function _get_amimoto_plugin_description( $plugin_name ) {
-		switch ( $plugin_name ) {
-			case 'Nginx Cache Controller':
-				$description = __( 'Provides some functions of controlling Nginx proxy server cache.', self::$text_domain );
-				break;
-
-			case 'Nephila clavata':
-				$description = __( 'Allows you to mirror your WordPress media uploads over to Amazon S3 for storage and delivery.', self::$text_domain );
-				break;
-
-			case 'C3 Cloudfront Cache Controller':
-				$description = __( "Controlle CloudFront's CDN server cache.", self::$text_domain );
-				break;
-
-			default:
-				$description = '';
-				break;
-		}
-		return $description;
-	}
-
-	/**
-	 *  Get amimoto plugin description for use
-	 *
-	 * @access private
-	 * @param (string) $plugin_name
-	 * @return string
-	 * @since 0.0.1
-	 */
-	private function _get_amimoto_plugin_for_use( $plugin_name ) {
-		switch ( $plugin_name ) {
-			case 'nginxchampuru':
-			case 'Nginx Cache Controller':
-				$description  = __( 'Nginx Reverse Proxy Cache', self::$text_domain );
-				break;
-
-			case 'nephila-clavata':
-			case 'Nephila clavata':
-				$description  = __( 'Amazon S3', self::$text_domain );
-				break;
-
-			case 'c3-cloudfront-clear-cache':
-			case 'C3 Cloudfront Cache Controller':
-				$description  = __( 'Amazon CloudFront', self::$text_domain );
-				break;
-
-			default:
-				$description = '';
-				break;
-		}
-		return $description;
 	}
 
 	/**
