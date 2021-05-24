230728cb423045f7d148d83e76a0b7c7c938b561
Hidetaka Okamoto <info@wp-kyoto.net>
2021-05-25 00:10:52 +0900

    fix: test

--- tests/Environment_Test.php
+++ tests/Environment_Test.php
@@ -1,14 +1,15 @@
 <?
 namespace AMIMOTO_Dashboard\Tests;
 
-use AMIMOTO_Dashboard\Constants;
+use AMIMOTO_Dashboard\WP\Environment;
 
-class Constants_Test extends \WP_UnitTestCase {
+class Environment_Test extends \WP_UnitTestCase {
     /**
      * @dataProvider provide_is_amimoto_managed_test_case
      */
     public function test_is_amimoto_managed( $server, $expected ) {
-        $this->assertEquals( $expected, Constants::is_amimoto_managed( $server ) );
+        $environment = new Environment();
+        $this->assertEquals( $expected, $environment->is_amimoto_managed( $server ) );
     }
     public function provide_is_amimoto_managed_test_case() {
         return [
