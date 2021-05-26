f2707233e028586b66410e50b9a37726e6931c9c
Hidetaka Okamoto <info@wp-kyoto.net>
2021-05-26 12:51:22 +0900

    fix: remove self

--- classes/WP/Plugins.php
+++ classes/WP/Plugins.php
@@ -237,7 +237,7 @@ class Plugins {
         $text_domain = Constants::text_domain();
 		switch ( $plugin_name ) {
 			case 'Nginx Cache Controller':
-				$description = __( 'Provides some functions of controlling Nginx proxy server cache.', self::$text_domain );
+				$description = __( 'Provides some functions of controlling Nginx proxy server cache.', $text_domain );
 				break;
 
 			case 'C3 Cloudfront Cache Controller':
